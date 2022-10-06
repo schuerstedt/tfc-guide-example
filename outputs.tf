c845b215d379b78e2e48b46c14c98439ffd9fe7a
schuerstedt <marcus@schuerstedt.com>
2022-10-06 16:18:09 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,12 @@
-output "instance_ami" {
-  value = aws_instance.ubuntu.ami
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
 }
 
-output "instance_arn" {
-  value = aws_instance.ubuntu.arn
+output "public_ip_address" {
+  value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
 }
 
+output "tls_private_key" {
+  value     = tls_private_key.example_ssh.private_key_pem
+  sensitive = true
+}
